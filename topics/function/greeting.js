59de91b99a3bb7fbb95bc430c8b409b9f0e2c443
Sonu Yadavvv <shidhu221@gmail.com>
2025-02-24 08:42:13 +0000

    @fn.1:Name print

---
+++ topics/function/greeting.js
@@ -0,0 +1,10 @@
+//1.Greeting Function: Write a function that takes a name as
+//input and returns a greeting message (e.g., "Hello, [name]!").
+//
+
+function greeting(name)
+{
+    console.log(`Hello,${name}!`);
+}
+
+greeting('Devil');
