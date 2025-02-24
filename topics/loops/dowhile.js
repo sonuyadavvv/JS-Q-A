5fc728ff3ac030e4eb14af611c8a673a14edb6c0
Sonu Yadavvv <shidhu221@gmail.com>
2025-02-24 08:31:27 +0000

    do while loop

---
+++ topics/loops/dowhile.js
@@ -0,0 +1,10 @@
+//10.Do...While Example: Write a program that uses a do...while loop
+//to repeatedly ask the user for a number until they enter a positive number.
+
+let i=-5
+do
+{
+    console.log(i);
+    i++;
+}
+while(i<=0)
